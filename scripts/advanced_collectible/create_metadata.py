bd6aac347ea9787fdabf2f631b499bf902c3b0b6
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-04-22 08:20:43 +0100

    done

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -3,6 +3,7 @@ from scripts.helpful_scripts import get_breed
 from metadata.sample_metadata import metadata_template
 from pathlib import Path
 import requests
+import json
 
 def main():
     advanced_collectible = AdvancedCollectible[-1]
@@ -21,14 +22,18 @@ def main():
             print(collectible_metadata)
             image_path = "./img/" + breed.lower().replace("_", "-") + ".png"
             image_uri = upload_to_ipfs(image_path)
+            collectible_metadata["image"] = image_uri
+            with open(metadata_file_name, "w") as file:
+                json.dump(collectible_metadata, file)
+            upload_to_ipfs(metadata_file_name)
 
 def upload_to_ipfs(filepath):
     with Path(filepath).open("rb") as fp:
         image_binary = fp.read()
-        ipfs_url = ""
+        ipfs_url = "http://127.0.0.1:5001"
         endpoint = "/api/v0/add"
         response = requests.post(ipfs_url + endpoint, files="file": image_binary)
-        ipfs_hash = response.json()[]
+        ipfs_hash = response.json()["Hash"]
         filename = filepath.split("/")[-1:][0]
         image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
         print(image_uri)
